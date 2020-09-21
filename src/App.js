5f67ab586925a904657815f41a800eddb1205cd6
romansyhinskiy <romansyhinskiy@gmail.com>
2020-09-21 09:37:51 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Layout from './hoc/Layout/Layout'
+
+function App() {
+  return (
+    <Layout>
+      <h1>Hello</h1>
+    </Layout>
+  );
+}
+
+export default App;
